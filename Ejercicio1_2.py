ddced3a107d5b90882afb2e07d31db01166e0efc
Gregorio Coronado <gregorio.coronado@iescampanillas.com>
2020-12-20 19:51:56 +0100

    v0.1

---
+++ Ejercicio1_2.py
@@ -0,0 +1,17 @@
+# Ejercicio 2, Apartado 1.
+# Del libro Aprendiendo Python desde 0. Jon Vadillo. 2020
+# Autor. Gregorio Coronado Morón.
+# Fecha. 12-2020
+
+
+# Esto es un comentario
+# Nombres de vbles. siempre empiezan por letras o _
+
+nombre = input("Introduce tu nombre> ")
+dni = input("Introduce tu DNI> ")
+edad = input("Introduce tu edad> ")
+
+print(f"Tu nombre es: {nombre}")
+print("Tu DNI es:" + dni)
+print("Tienes "+ edad + " años")
+
