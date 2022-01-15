a5d201bfbdb8664e1657efd9d2577015154465b0
pulsaf1 <gregorio.coronado@iescampanillas.com>
2022-01-15 17:45:19 -0500

    v0.6

--- src/Ejercicio_Cap8_1.py
+++ src/Ejercicio_Cap8_1.py
@@ -4,19 +4,19 @@
 # Fecha. 12-2020
 
 # Un numero es primo cuando solo es divisible por si mismo y por el 1.
-# Versión con un UNICO RETURN en la función.
+# Versión con un UNICO RETURN en la función y que levanta una excepción si el
+# parámetro no es un entero o es un número negativo. 
 
 def esPrimo(x):
     if x < 0:
         raise TypeError("El numero no puede ser negativo") 
 
     divisores = 1 
-    if isinstance(x,int): 
-        if x >= 2:
-            divisores = 0
-            num = 2
-            while (num < x and divisores == 0):
-                if (x % num == 0): divisores+=1
-                num += 1
+    if x >= 2:
+        divisores = 0
+        num = 2
+        while (num < x and divisores == 0):
+            if (x % num == 0): divisores+=1
+            num += 1
     return (divisores == 0)    
 
